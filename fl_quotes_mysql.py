fe97e9d2e1b411ca9dca9c88d4829281d71b0af0
Jisnu Prabhu <29878127+jisnup@users.noreply.github.com>
2019-01-11 16:21:33 -0500

    Comments were added.

--- fl_quotes_mysql.py
+++ fl_quotes_mysql.py
@@ -1,39 +1,47 @@
-from sqlalchemy import create_engine
+"""
+Author: Jisnu Prabhu
+Date: 01/11/2019
+This code loads the flat file and creates and inserts the data into tempory tables. It checks if the duplicates are present.
+It deletes the duplicates and inserts the rows to quotes table.
+"""
+# Packages
+from sqlalchemy import create_engine # Connect to database
 import mysql.connector
 import pandas as pd
-from openpyxl import load_workbook
+from openpyxl import load_workbook # write to excel
 from datetime import datetime as dt
 import os
-import glob
-import time
-import shutil
+import glob # For pattern matching
+import time 
+import shutil # To move files
 
+# Calculate excute time
 start_time = time.time()
-
+#Connecting to the server db
 engine = create_engine('mysql+mysqlconnector://root:TTIdb@localhost:3306/sample', echo = False)
 conn = engine.connect()
 
-# P&C Total Serious Quotes Detail
+# File to insert to db(Daily Quotes)
 file_name = 'C:/Users/jisnu/Desktop/jisnu/data/P&C Total Serious Quotes Detail.xlsx'
-df = pd.read_excel(file_name,skiprows = 10)
 #file_name = 'C:/Users/jisnu/Desktop/jisnu/data/fl_quote.xlsx'
+df = pd.read_excel(file_name,skiprows = 10)
 #df = pd.read_excel(file_name,nrows = 37500)
 
-#df.drop(['Unnamed: 0','Unnamed: 12'],axis = 1 , inplace = True)
-#df.drop(df.index[len(df) - 1], axis = 0, inplace = True)
-
+# File coems from Allstate (bad formatting)
+df.drop(['Unnamed: 0','Unnamed: 12'],axis = 1 , inplace = True)
+df.drop(df.index[len(df) - 1], axis = 0, inplace = True)
+# Changing columns names 
 df.columns = [c.lower() for c in df.columns]
 df.columns = [c.replace(" ", "_") for c in df.columns]
-
-df.drop(['agent_number','customer_state','customer_zip_code','channel'], axis =1, inplace = True)
 df.rename(columns={'quote_control_number' : 'quote_number'}, inplace=True)
+# Adding zero's to subproducers code (empolyee table has no, code)
 df.loc[df.sub_producer == 'No Code', 'sub_producer'] = '000 - No Code'
-df['sub_producer'] = df['sub_producer'].str[6:]
-df[['sales_first_name', 'sales_last_name']] = df['sub_producer'].str.split(" ", 1, True)
-df[['customer_last_name', 'customer_first_name']] = df['customer_name'].str.split(",", 1, True)
-df.drop(['customer_name', 'sub_producer'], axis = 1 , inplace = True)
+df['sub_producer'] = df['sub_producer'].str[6:] # Sub code gets recycled 
+df[['sales_first_name', 'sales_last_name']] = df['sub_producer'].str.split(" ", 1, True) # link with employee name 
+df[['customer_last_name', 'customer_first_name']] = df['customer_name'].str.split(",", 1, True) # can't link, customer not in customer table yet
+df.drop(['customer_name', 'sub_producer','agent_number','customer_state','customer_zip_code','channel'], axis = 1 , inplace = True)
 df = df[['quote_number','sales_first_name','sales_last_name','customer_first_name','customer_last_name', 'production_date', 'product', 'quoted_item_count', 'quoted_premium']]
-
+# Loads data into a tempory file.
 df.to_sql(name = 'fl_quotes_temp', con = engine, if_exists = 'append', index = False)
 
 conn.execute(
@@ -50,7 +58,7 @@ CREATE TABLE IF NOT EXISTS fl_quotes_temp(
 	quoted_premium FLOAT(8,2))
     ENGINE=INNODB;
 """)
-
+# TODO: Remove the duplicates
 conn.execute(
 """
 DELETE FROM fl_quotes_temp
@@ -67,7 +75,7 @@ DELETE FROM fl_quotes_temp
     ) AS c
 )
 """)
-
+# TODO: Work on join statement (ON)
 conn.execute(
 """
 INSERT INTO fl_quotes(
@@ -75,23 +83,22 @@ INSERT INTO fl_quotes(
 	SELECT q.quote_number, e.license, q.customer_first_name, q.customer_last_name, q.production_date, q.product , q.quoted_item_count , q.quoted_premium 
 	FROM fl_quotes_temp AS q
 	JOIN employee AS e
-	WHERE (e.first_name = q.sales_first_name AND e.last_name = q.sales_last_name) 
+	ON (e.first_name = q.sales_first_name AND e.last_name = q.sales_last_name) 
 """)
 
 conn.execute(""" DROP TABLE fl_quotes_temp; """)
 
-d = dt.now().date()
-if d.day < 10:
-  dd = '0'+str(d.day)
+# string with the year+month+day for current day 
+today = dt.now().date()
+if today.day < 10:
+  dayNo = '0'+str(today.day)
 else:
-  dd = str(d.day)
-day = str(d.year)+str(d.month)+dd
+  dayNo= str(today.day)
+day = str(today.year)+str(today.month)+ dayNo
 
-"""
-new_file = "C:/Users/Jisnu/Desktop/jisnu/Extra/business_mysql_"+day+".py"
-shutil.move("C:/Users/Jisnu/Desktop/jisnu/Data/business_mysql.py", new_file)
-print('File inserted and move to Archive!!')
-"""
-print('Insert compelete after: {} seconds'.format(time.time() - start_time))
+# Moving the file to archive after insertion with the date for the tracking
+location_file = "C:/Users/Jisnu/Desktop/jisnu/Extra/business_mysql_"+day+".py"
+shutil.move("C:/Users/Jisnu/Desktop/jisnu/Data/business_mysql.py", location_file)
+print('Insert compelete after: {} seconds File inserted and move to Archive!!'.format(time.time() - start_time))
 
 # Deal with dup
